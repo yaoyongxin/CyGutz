6a54b1ff0d6d382795150f7982156ac903fb64c3
Yongxin Yao <CyGutz@gmail.com>
2015-01-19 17:17:25 -0600

    remove interface to wien2k

--- Interface_Wien2K/chargesc/ph.f
+++
@@ -1,8 +0,0 @@
-      REAL*8 FUNCTION PH(N)                                             
-      IMPLICIT REAL*8 (A-H,O-Z)
-      PH=1.0D0                                                          
-      K=MOD(IABS(N),4)                                                  
-      IF(K.EQ.0) GOTO 5                                                 
-      PH=-1.0D0                                                         
-    5 RETURN                                                            
-      END                                                               
