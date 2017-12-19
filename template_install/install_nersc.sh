69242dec4c1e2dd909d7e320e8bb516196dd10c8
Yongxin Yao <yxcode@gmail.com>
2017-12-19 11:17:54 -0600

    added installation script for nersc

--- template_install/install_nersc.sh
+++ template_install/install_nersc.sh
@@ -8,7 +8,7 @@ echo "Build CyGutz and install to ${GUTZ_ROOT2}"
 source ${HOME}/.bashrc
 if [ "${GUTZ_ROOT2}" != "${WIEN_GUTZ_ROOT2}" ]; then 
   echo "# CyGutz" >> ~/.bashrc;
-  echo "export WIEN_GUTZ_ROOT2=${GUTZ_ROOT2}" >> ~/.bashrc; 
+  echo "export WIEN_GUTZ_ROOT2=${GUTZ_ROOT2}" >> ~/.bashrc.ext; 
   echo "export PATH=\${WIEN_GUTZ_ROOT2}:\${PATH}" >> ~/.bashrc;
   source ${HOME}/.bashrc
 fi
