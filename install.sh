47adf5a38e55d6479fa20ffc6e1fbf5e22529c66
Yongxin Yao <yxcode@gmail.com>
2017-12-19 09:16:41 -0600

    modify install.sh to include wine_gutz/bin2 into path.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "Build CyGutz and install to ${GUTZ_ROOT2}"
 source ${HOME}/.bashrc
 if [ "${GUTZ_ROOT2}" != "${WIEN_GUTZ_ROOT2}" ]; then 
   echo "export WIEN_GUTZ_ROOT2=${GUTZ_ROOT2}" >> ~/.bashrc; 
-  echo "export PATH=${GUTZ_ROOT2}:${PATH}" >> ~/.bashrc;
+  echo "export PATH=\${WIEN_GUTZ_ROOT2}:\${PATH}" >> ~/.bashrc;
   source ${HOME}/.bashrc
 fi
 
