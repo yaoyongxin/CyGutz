0c50f5c0c5cdfb90a88e24b83dae0fd14f26364f
Yongxin Yao <ykent@cori12.nersc.gov>
2017-12-22 06:24:38 -0800

    fixed install_nersc.sh

--- template_install/install_nersc.sh
+++ template_install/install_nersc.sh
@@ -5,12 +5,12 @@ echo
 GUTZ_ROOT2=${HOME}/WIEN_GUTZ/bin2
 echo "Build CyGutz and install to ${GUTZ_ROOT2}"
 
-source ${HOME}/.bashrc
+source ${HOME}/.bashrc.ext
 if [ "${GUTZ_ROOT2}" != "${WIEN_GUTZ_ROOT2}" ]; then 
-  echo "# CyGutz" >> ~/.bashrc;
+  echo "# CyGutz" >> ~/.bashrc.ext;
   echo "export WIEN_GUTZ_ROOT2=${GUTZ_ROOT2}" >> ~/.bashrc.ext; 
-  echo "export PATH=\${WIEN_GUTZ_ROOT2}:\${PATH}" >> ~/.bashrc;
-  source ${HOME}/.bashrc
+  echo "export PATH=\${WIEN_GUTZ_ROOT2}:\${PATH}" >> ~/.bashrc.ext;
+  source ${HOME}/.bashrc.ext
 fi
 
 make install
