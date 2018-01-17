d743dd79c3a70bd58b7d6612767b195aa53023f6
Yong-Xin Yao <cygutz@gmail.com>
2018-01-17 12:29:40 -0800

    added dc around mean field, which may give deeper Vdc.

--- pyglib/pyglib/mbody/double_counting.py
+++ pyglib/pyglib/mbody/double_counting.py
@@ -21,3 +21,19 @@ def get_vdc_fll(uavg, javg, ntot):
     '''
     vdc = -uavg*(ntot-0.5) + javg/2*(ntot-1)
     return vdc
+
+
+def get_vdc_amf(uavg, javg, ntot, l):
+    '''get double counting potential in the around mean-field form.
+    The usual minus sign is included.
+    '''
+    alpha = -uavg/2.+(uavg+2*l*javg)/(2*l+1)/4.
+    vdc = alpha*2*ntot
+    return vdc
+
+
+
+if __name__ == "__main__":
+    uavg, javg, ntot, l = 6.0, 0.8, 5.2, 3
+    print(get_vdc_fll(uavg, javg, ntot))
+    print(get_vdc_amf(uavg, javg, ntot, l))
